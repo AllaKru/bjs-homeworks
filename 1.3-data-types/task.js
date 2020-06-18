4d834954717c43cc2b8ca63c2a397f4071d3fa26
Alla Kruglova <renata-l@yandex.ru>
2020-06-18 17:04:30 +0300

    New homework 1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  if (isNaN(parseFloat(percent))) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}.`;
+  } else if (isNaN(parseFloat(contribution))) {
+    return `Параметр "Cумма первоначального взноса" содержит неправильное значение ${contribution}.`;
+  } else if (isNaN(parseFloat(amount))) {
+    return `Параметр "Cумма кредита" содержит неправильное значение ${amount}.`;
+  } else if (isNaN(date)) {
+    return `Параметр "Дата окончания кредита" содержит неправильное значение ${date}.`;
+  } else {
+    let bodyCredit = amount - contribution;
+    let months =
+      (date.getFullYear() - new Date().getFullYear()) * 12 +
+      (date.getMonth() - new Date().getMonth());
+    let P = percent / 12 / 100;
+    let monthAmount = bodyCredit * (P + P / (Math.pow(1 + P, months) - 1));
+    let totalAmount = parseFloat((monthAmount * months).toFixed(2));
+    console.log(totalAmount);
+    return totalAmount;
+  }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if (!name || name === "null" || name === "undefined") {
+    name = "Аноним";
+  }
+  let greeting = `Привет, мир! Меня зовут ${name}`;
+  console.log(greeting);
+  return greeting;
 }
