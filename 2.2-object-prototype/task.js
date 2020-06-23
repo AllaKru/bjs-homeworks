816ee44212c60a6534b9199693624e074390fe85
Alla Kruglova <renata-l@yandex.ru>
2020-06-24 00:30:39 +0300

    Homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
+String.prototype.isPalindrome = function () {
+  let newstr = this.toLowerCase().replace(new RegExp(" ", "g"), "");
+  let newreverse = newstr.split("").reverse().join("");
+
+  return newstr === newreverse;
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let summ = 0;
+  if (marks.length === 0 || Number.isNaN(marks)) {
+    return 0;
+  }
+  for (let i = 0; i < marks.length; i++) {
+    summ += marks[i];
+  }
+  let average = parseFloat(summ / marks.length);
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now(); //Number(new Date())// +new Date()
+  let birth = Date.parse(birthday);
+  let diff = now - birth;
+  let age = diff / 31557600000;
+  // let age = (now -(diff/4 + birth))/31557600000;
+  let verdict = age > 18 ? true : false;
+  return verdict;
 }
